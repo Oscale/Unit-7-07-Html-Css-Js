7736f156bb6b216ae8972264be81a9eee98dc717
Oscar A.G <>
2021-05-12 15:18:49 +0000

    Indentation on Js file

--- script.js
+++ script.js
@@ -14,25 +14,25 @@ function CalCulate () {
 
   anwser1 = 0
 
-  if ((n1 >= 0) && (n2 >=0)) { 
+  if ((n1 >= 0) && (n2 >= 0)) {
     for (times = 0; times < n1; times++) {
       anwser1 = n2 + anwser1 
-    } 
+    }
   } else if ((n1 < 0) && (n2 < 0)) {
     n1 = Math.abs(n1)
     n2 = Math.abs(n2)
 
     for (times = 0; times < n1; times++) {
-      anwser1 = n2 + anwser1 
-    } 
+      anwser1 = n2 + anwser1
+    }
   } else {
     n1 = Math.abs(n1)
     n2 = Math.abs(n2)
 
     for (times = 0; times < n1; times++) {
-      anwser1 = n2 + anwser1 
-    } 
+      anwser1 = n2 + anwser1
+    }
     anwser1 = 0 - anwser1
   }
- document.getElementById('result').innerHTML = anwser1
+  document.getElementById('result').innerHTML = anwser1
 }
